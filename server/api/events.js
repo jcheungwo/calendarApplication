fe2d6e74c49600d87d3962c93df4aa8ea6a689fe
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-06-21 21:48:22 -0400

    Feature: get/post/delete/put route for single event

--- server/api/events.js
+++ server/api/events.js
@@ -4,6 +4,30 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   Event.findAll()
-    .then(users => res.json(users))
-    .catch((err) => console.log(err))
+  .then(events => res.json(events))
+  .catch((err) => console.log(err))
+})
+
+router.get('/:id', (req, res, next) => {
+  Event.findOne({where: {id: req.params.id}})
+  .then(event => res.json(event))
+  .catch((err) => console.log(err))
+})
+
+router.post('/', (req, res, next) => {
+  Event.create(req.body)
+  .then(newEvent => res.json(`${newEvent.title} has been created`))
+  .catch((err) => console.log(err))
+})
+
+router.delete('/:id', (req, res, next) => {
+  Event.destroy({where: {id: req.params.id}})
+  .then(() => res.json(`Event with id number ${req.params.id} has been deleted`))
+  .catch((err) => console.log(err))
+})
+
+router.put('/:id', (req, res, next) => {
+  Event.update(req.body, {where: {id: req.params.id}})
+  .then(() => res.json(`Event with id number ${req.params.id} has been updated`))
+  .catch((err) => console.log(err))
 })
